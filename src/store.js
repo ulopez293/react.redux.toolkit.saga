384c4e9f76a8294cda41b8cb1c7f863c8eb59ea9
ulopez <ulopez293@gmail.com>
2022-07-28 18:04:36 -0500

    expire state

--- src/store.js
+++ src/store.js
@@ -3,28 +3,24 @@ import detalleReducer from './slices/detalleSlice'
 import checkReducer from './slices/checkSlice'
 import loginReducer from './slices/loginSlice'
 import { combineReducers } from 'redux'
-
 import createSagaMiddleware from 'redux-saga'
 import sagas from './sagas'
-
-import storage from 'redux-persist/lib/storage'
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from 'redux-persist'
+    persistStore, persistReducer,
+    FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER,
+} from 'redux-persist'
+import expireIn from "redux-persist-transform-expire-in";
+import storage from 'redux-persist/lib/storage'
 
 let sagaMiddleware = createSagaMiddleware()
 const middleware = [sagaMiddleware]
 
+const expireMiliSeconds = 1000 * (60 * 60) // 1 hour
+const expirationKey = "persistencyExpiration"
 const persistConfig = {
     key: 'root',
     storage,
+    transforms: [expireIn(expireMiliSeconds, expirationKey, [])]
 }
 
 const rootReducer = combineReducers({
