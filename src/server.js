ff5cb7263e99aabfbd1fd058c1fbb9a64fe07f08
ulopez <ulopez293@gmail.com>
2022-09-08 20:09:26 -0500

    server config env

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@
 const configuracionServidor = {
     //host: "http://127.0.0.1:8000/api/",
     host: import.meta.env.VITE_API_URL,
-    dugrop: 'http://localhost/DUGROP/public/api/'
+    dugrop: import.meta.env.VITE_DUGROP_URL, //'http://localhost/DUGROP/public/api/'
     //host: (window.location.origin) ? '' : ''
     //host: "http://192.108.24.197:8046/api/"
 }
